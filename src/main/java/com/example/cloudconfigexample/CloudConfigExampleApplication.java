fdea1736795a9c1e6e768648f4d4c5d6d6637a70
Ali Yalama <aliyalama@googlemail.com>
2023-03-11 16:17:26 +0100

    Initial commit

---
+++ src/main/java/com/example/cloudconfigexample/CloudConfigExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudconfigexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigExampleApplication.class, args);
+    }
+
+}
